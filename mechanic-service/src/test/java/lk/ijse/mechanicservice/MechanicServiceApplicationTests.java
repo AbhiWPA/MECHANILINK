ee083df56a2a6f5a07468b4f74e55ab9916c26b9
abhia <abhiashinsa770@gmail.com>
2025-11-21 04:17:39 +0400

    mechanic service created

---
+++ mechanic-service/src/test/java/lk/ijse/mechanicservice/MechanicServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.mechanicservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MechanicServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
