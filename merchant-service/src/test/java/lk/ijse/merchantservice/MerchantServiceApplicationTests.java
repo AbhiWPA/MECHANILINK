aa21783d5560a3d22b2b06065d574f4c938ef81f
abhia <abhiashinsa770@gmail.com>
2025-11-21 04:22:58 +0400

    merchant service created

---
+++ merchant-service/src/test/java/lk/ijse/merchantservice/MerchantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.merchantservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchantServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
